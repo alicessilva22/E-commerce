4693d159c4e99628b5eb0896d49fcf3733824477
Alice Silva <alicessilva@aol.com>
2022-06-30 17:57:49 -0400

    category-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -6,13 +6,33 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 // get all products
 router.get('/', (req, res) => {
   // find all products
-  // be sure to include its associated Category and Tag data
+  try {
+    const prodData = await Product.findAll({
+    // be sure to include its associated Category and Tag data
+      include: [{ model: Category }, { model: Tag }],
+    });
+    res.status(200).json(prodData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
+  try {
+    const proData = await Product.findByPk(req.params.id, {
+     // be sure to include its associated Category and Tag data
+      include: [{ model: Category}, { model: Tag }],
+    });
+    if (!proData) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(proData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 // create new product
