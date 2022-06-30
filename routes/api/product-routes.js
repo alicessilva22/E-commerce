68f5e12e18e1058867986df56d1224e720e88c3d
Alice Silva <alicessilva@aol.com>
2022-06-30 18:16:03 -0400

    product-routes

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -45,7 +45,17 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+  },
+  {
+    where: {
+
+    },
+  }
+    )
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -111,6 +121,15 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then((deletedProduct) => {
+    res.json(deletedProduct);
+  })
+  .catch((err) => res.json(err));
 });
 
 module.exports = router;
